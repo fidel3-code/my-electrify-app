f6e2befd3d9603960569cb4e1cdd52b1f49d6665
lonewolf-sar <karbolojoshua@gmail.com>
2026-04-10 13:34:20 +0300

    created the simba sokogarden app

---
+++ app/src/main/java/com/fidel/simbasokogaren/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.fidel.simbasokogaren
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+
+        val signin = findViewById<Button>(R.id.signin)
+        signin.setOnClickListener {
+
+            val signinIntent= Intent(applicationContext,signin::class.java)
+            startActivity(signinIntent)
+        }
+
+        val signup = findViewById<Button>(R.id.signup)
+        signup.setOnClickListener {
+
+            val signupIntent= Intent(applicationContext,signup::class.java)
+            startActivity(signupIntent)
+        }
+    }
+
+
+
+
+
+}
---
+++ farasi_joshua/app/src/main/java/com/joshua/farasi_joshua/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.joshua.farasi_joshua
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.joshua.farasi_joshua.ui.theme.Farasi_joshuaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Farasi_joshuaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Farasi_joshuaTheme {
+        Greeting("Android")
+    }
+}
