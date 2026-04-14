6cfa658cdf664d2036880c02869d6030de70b8cf
lonewolf-sar <karbolojoshua@gmail.com>
2026-04-14 12:47:53 +0300

    updated the app

--- app/src/main/java/com/fidel/simbasokogaren/SignUp.kt
+++ app/src/main/java/com/fidel/simbasokogaren/SignUp.kt
@@ -1,10 +1,15 @@
 package com.fidel.simbasokogaren
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,32 @@ class SignUp : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signup=findViewById<TextView>(R.id.signup_link)
+
+        signup.setOnClickListener {
+            val signUplink= Intent(applicationContext, SignIn::class.java)
+            startActivity(signUplink)
+
+        }
+
+        val username=findViewById<EditText>(R.id.username)
+        val email=findViewById<EditText>(R.id.email)
+        val phone=findViewById<EditText>(R.id.phone)
+        val password=findViewById<EditText>(R.id.password)
+        val signupbutton=findViewById<Button>(R.id.signup)
+        signupbutton.setOnClickListener {
+            val api="http://fidel.alwaysdata.net/api/signup"
+            val data= RequestParams()
+
+            data.put("username",username.text.toString())
+            data.put("email",email.text.toString())
+            data.put("phone",phone.text.toString())
+            data.put("password",password.text.toString().trim())
+
+            val helper= ApiHelper(applicationContext)
+            helper.post(api,data)
         }
     }
 }
